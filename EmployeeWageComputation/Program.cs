ac4fee4293ab26a131155b3c8c8971e5e36b2fb2
imransk7768 <imransk7768@gmail.com>
2022-04-08 15:28:28 +0530

    [Imran] Add : Add SwitchCase_PartTime_EmpWage Implementation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWageComputation;
 WageComputation com = new WageComputation();
-//com.EmployeeAttendence();
-//com.CalculateEmpWage();
-com.CalculateEmpDailyWage();
+com.EmpDailyWage();
--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -11,26 +11,26 @@ namespace EmployeeWageComputation
         const int IS_FullTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20, Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4;
         int empHrs = 0, totalEmpWage;
 
-        public void CalculateEmpDailyWage()
+        public void EmpDailyWage()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FullTIME)
+            switch (empCheck)
             {
-                Console.WriteLine("Employee Working as a Full Time");
-                empHrs = Emp_Full_Time_Hrs;
-            }
-            else if (empCheck == IS_PARTTIME)
-            {
-                Console.WriteLine("Employee Working as a Part Time");
-                empHrs = Emp_Part_time_Hrs;
+                case IS_FullTIME:
+                    Console.WriteLine("Employee Working as a Full Time");
+                    empHrs = Emp_Full_Time_Hrs;
+                    break;
+                case IS_PARTTIME:
+                    Console.WriteLine("Employee Working as a Part Time");
+                    empHrs = Emp_Part_time_Hrs;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
 
-            else
-            {
-                empHrs = 0;
-            }
             totalEmpWage = empHrs * EmpRatePerHr;
             Console.WriteLine("Day Employee Salary is : " + totalEmpWage);
         }
