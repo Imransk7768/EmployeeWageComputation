6668524d52184524e18ad581299987adee9ea9b0
imransk7768 <imransk7768@gmail.com>
2022-04-08 19:30:34 +0530

    [Imran] Add : EmplMonthlyWage_SwitchCase Implemented

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,31 +8,36 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int IS_FullTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20, Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4;
-        int empHrs = 0, totalEmpWage;
 
-        public void EmpDailyWage()
+        const int IS_FullTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20,
+        Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4, Emp_Working_Per_Month = 20;
+        int empHrs = 0, totalEmpSalary = 0;
+
+        public void EmpMonthlyWage()
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            for (int i = 0; i <= Emp_Working_Per_Month; i++)
             {
-                case IS_FullTIME:
-                    Console.WriteLine("Employee Working as a Full Time");
-                    empHrs = Emp_Full_Time_Hrs;
-                    break;
-                case IS_PARTTIME:
-                    Console.WriteLine("Employee Working as a Part Time");
-                    empHrs = Emp_Part_time_Hrs;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
 
-            totalEmpWage = empHrs * EmpRatePerHr;
-            Console.WriteLine("Day Employee Salary is : " + totalEmpWage);
+                switch (empCheck)
+                {
+                    case IS_FullTIME:
+
+                        empHrs += Emp_Full_Time_Hrs;
+                        break;
+                    case IS_PARTTIME:
+
+                        empHrs += Emp_Part_time_Hrs;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+            }
+            totalEmpSalary = empHrs * EmpRatePerHr;
+            Console.WriteLine("Monthly Employee salary is :" + totalEmpSalary);
         }
     }
 }
