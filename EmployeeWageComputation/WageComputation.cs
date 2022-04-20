e1f1ef7c1fb16ff83bb9c904b0ae55932af4ec53
Imransk7768 <imransk7768@gmail.com>
2022-04-20 15:02:36 +0530

    [Imran] Refactor: EmpMonthlyWage Using ClassMethods & ClassVariables

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,39 +8,39 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-
-        const int IS_FullTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20,
+        const int IS_FUllTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20,
         Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4, Emp_Working_Per_Month = 20,
         Max_Work_Hr = 100;
-        int empHrs = 0, totalEmpSalary = 0, day = 0;
-
+        int empHrs = 0, day = 0, salary, sum, totalEmpSalary;
         public int Max_Working_Hr { get; set; }
-
         public void MaxEmpMonthlyWage()
         {
-            while (day <= Emp_Working_Per_Month && empHrs <= Max_Work_Hr)
+            for (int i = 1; i <= 20; i++)
             {
                 Random random = new Random();
-                int empCheck = random.Next(0, 3);
-
-                switch (empCheck)
+                int check = random.Next(0, 3);
+                switch (check)
                 {
-                    case IS_FullTIME:
-
-                        empHrs += Emp_Full_Time_Hrs;
+                    case IS_FUllTIME:
+                        empHrs = 8;
                         break;
                     case IS_PARTTIME:
-
-                        empHrs += Emp_Part_time_Hrs;
+                        empHrs = 4;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                day++;
+                salary = empHrs * EmpRatePerHr;
+                Console.WriteLine("Day" + i + " " + salary);
+                totalEmpSalary = totalEmpSalary + salary;
+                if (empHrs <= 100)
+                {
+                    sum = sum + empHrs;
+                }
             }
-            totalEmpSalary = empHrs * EmpRatePerHr;
-            Console.WriteLine("month Employee Salary is :" + totalEmpSalary);
+            Console.WriteLine("Total Salary of 20 Days : " + totalEmpSalary);
+            Console.WriteLine("Total Working Hrs is : " + sum);
         }
     }
 }
