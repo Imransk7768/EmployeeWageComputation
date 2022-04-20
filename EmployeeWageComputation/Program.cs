33446cebd6c7a0d1d19c9334520caedafd7aeb87
Imransk7768 <imransk7768@gmail.com>
2022-04-20 15:56:37 +0530

    [Imran] Added: EmpWage For Multiple Company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWageComputation;
 WageComputation com = new WageComputation();
-com.MaxEmpMonthlyWage();
+com.multipleCompanyWage("Google", 20, 2, 10);
+com.multipleCompanyWage("Microsoft", 10, 4, 20);
+com.multipleCompanyWage("Deloitte", 15, 3, 15);
--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,39 +8,35 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int IS_FUllTIME = 1, IS_PARTTIME = 2, EmpRatePerHr = 20,
-        Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4, Emp_Working_Per_Month = 20,
-        Max_Work_Hr = 100;
-        int empHrs = 0, day = 0, salary, sum, totalEmpSalary;
-        public int Max_Working_Hr { get; set; }
-        public void MaxEmpMonthlyWage()
+        const int IS_FUllTIME = 1, IS_PARTTIME = 2,
+        Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4;
+        int empHrs = 0, day = 0, totalEmpSalary;
+        public void multipleCompanyWage(string Company_Name, int Emp_Wage_Per_Hr, int Max_Work_Hr,
+            int Emp_Working_Per_Month)
         {
-            for (int i = 1; i <= 20; i++)
+            while (day <= Emp_Working_Per_Month && empHrs <= Max_Work_Hr)
             {
                 Random random = new Random();
                 int check = random.Next(0, 3);
                 switch (check)
                 {
                     case IS_FUllTIME:
-                        empHrs = 8;
+
+                        empHrs += Emp_Full_Time_Hrs;
                         break;
                     case IS_PARTTIME:
-                        empHrs = 4;
+
+                        empHrs += Emp_Part_time_Hrs;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                salary = empHrs * EmpRatePerHr;
-                Console.WriteLine("Day" + i + " " + salary);
-                totalEmpSalary = totalEmpSalary + salary;
-                if (empHrs <= 100)
-                {
-                    sum = sum + empHrs;
-                }
             }
-            Console.WriteLine("Total Salary of 20 Days : " + totalEmpSalary);
-            Console.WriteLine("Total Working Hrs is : " + sum);
+            totalEmpSalary = empHrs * Emp_Wage_Per_Hr;
+            Console.WriteLine("Company : " + Company_Name + ", Emp Working Hour : " + Emp_Wage_Per_Hr + 
+                ", Maximum working Hour : " + Max_Work_Hr + ", Total Days In Month : " + Emp_Working_Per_Month);
+            Console.WriteLine("Monthly Employee Salary is :" + totalEmpSalary);
         }
     }
 }
