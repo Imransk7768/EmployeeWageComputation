602631c9f289742a749cd45309d7f90cb71a2525
Imransk7768 <imransk7768@gmail.com>
2022-04-25 01:45:47 +0530

    [Imran] Added: Save Total Wage ForEach Company.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,6 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EmployeeWageComputation;
-WageComputation com = new WageComputation();
-com.multipleCompanyWage("Google", 20, 2, 10);
-com.multipleCompanyWage("Microsoft", 10, 4, 20);
-com.multipleCompanyWage("Deloitte", 15, 3, 15);
+﻿using System;
+namespace EmployeeWageComputation
+{
+    public interface Program
+    {
+        public static void Main(string[] args)
+        {
+            WageComputation Philips = new WageComputation("Philips", 20, 4, 10);
+            Philips.eachCompanyTotalWage();
+            Console.WriteLine(Philips.ToString());
+            WageComputation Samsung = new WageComputation("Samsung", 23, 8, 10);
+            Samsung.eachCompanyTotalWage();
+            Console.WriteLine(Samsung.ToString());
+            WageComputation Asus = new WageComputation("Asus", 25, 6, 12);
+            Asus.eachCompanyTotalWage();
+            Console.WriteLine(Asus.ToString());
+        }
+    }
+}
--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,35 +8,54 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int IS_FUllTIME = 1, IS_PARTTIME = 2,
-        Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4;
-        int empHrs = 0, day = 0, totalEmpSalary;
-        public void multipleCompanyWage(string Company_Name, int Emp_Wage_Per_Hr, int Max_Work_Hr,
-            int Emp_Working_Per_Month)
+        const int IS_FUllTIME = 1, IS_PARTTIME = 2, Emp_Full_Time_Hrs = 8, Emp_Part_time_Hrs = 4;
+
+        public string Company_Name;
+        int Emp_Wage_Per_Hr, Emp_Working_Per_Month, Max_Work_Hr;
+        int empHrs = 0, totalEmpSalary, day = 0, total_Emp_Hrs=0, emp_WageDaily;
+        decimal total_Emp_Wage;
+
+        public WageComputation(string Company_Name, int Emp_Wage_Per_Hr, int Emp_Working_Per_Month, int Max_Work_Hr)
+        {
+            this.Company_Name = Company_Name;
+            this.Emp_Wage_Per_Hr = Emp_Wage_Per_Hr;
+            this.Emp_Working_Per_Month = Emp_Working_Per_Month;
+            this.Max_Work_Hr = Max_Work_Hr;
+            this.total_Emp_Wage = total_Emp_Wage;
+        }
+
+        public void eachCompanyTotalWage()
         {
             while (day <= Emp_Working_Per_Month && empHrs <= Max_Work_Hr)
             {
                 Random random = new Random();
                 int check = random.Next(0, 3);
+
                 switch (check)
                 {
                     case IS_FUllTIME:
-
-                        empHrs += Emp_Full_Time_Hrs;
+                        empHrs = Emp_Full_Time_Hrs;
                         break;
                     case IS_PARTTIME:
-
-                        empHrs += Emp_Part_time_Hrs;
+                        empHrs = Emp_Part_time_Hrs;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
+                day++;
+                emp_WageDaily = empHrs * Emp_Wage_Per_Hr;
+                total_Emp_Hrs += empHrs;
+                total_Emp_Wage += emp_WageDaily;
             }
-            totalEmpSalary = empHrs * Emp_Wage_Per_Hr;
-            Console.WriteLine("Company : " + Company_Name + ", Emp Working Hour : " + Emp_Wage_Per_Hr + 
-                ", Maximum working Hour : " + Max_Work_Hr + ", Total Days In Month : " + Emp_Working_Per_Month);
-            Console.WriteLine("Monthly Employee Salary is :" + totalEmpSalary);
+            totalEmpSalary = total_Emp_Hrs * Emp_Wage_Per_Hr;
+            total_Emp_Wage = Emp_Wage_Per_Hr * Emp_Working_Per_Month;
+            Console.WriteLine("\nEmployee wage for days '" + day + "' is = " + emp_WageDaily + " for '" + empHrs + "' Hrs." );
+            Console.WriteLine("Total Employee Wage for company " + Company_Name + " is : " + totalEmpSalary);
+        }
+        public override string ToString()
+        {
+            return "Total Company Employee Wage of  " + this.Company_Name + " is : " + this.total_Emp_Wage + "\n";
         }
     }
 }
